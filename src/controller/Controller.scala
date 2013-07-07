06bdec4c6b7fae641434aac21deec1c925cdc998
Corey Brady <cbrady@inquirelearning.com>
2013-07-07 11:40:55 -0500

    cleanup comments

--- src/controller/Controller.scala
+++ src/controller/Controller.scala
@@ -21,12 +21,16 @@ class Controller(override protected val portName: String)
 
   def currentPort: Option[SerialPort] = portOpt
 
-  //@ c@ per googo board protocol doc, ping returns an ack PLUS 3 version bytes.  however, these are quite often corrupted/truncated.
-  //so we are just checking for the ack part in the writeAndWait() method (more rigorous check yielded many false failures on ping). CEB 7/3/13
+  //@ c@ per gogo board protocol doc, ping returns an ack PLUS 3 version bytes.  however, gogo models show lots of retries, 
+  // and my own testing shows frequent message fragmentation that falsely suggests failed pings. i have removed the validation
+  // of acks as a result. CEB 7/3/13
   def ping():           Boolean = portOpt map { _ => writeAndWait(CmdPing) } getOrElse false
   def beep():           Boolean = portOpt map { _ => writeAndWait(CmdBeep, 0x00.toByte) } getOrElse false
   def led(on: Boolean): Boolean = portOpt map { _ => writeAndWait(if (on) CmdLedOn else CmdLedOff, 0x00.toByte) } getOrElse false
 
+  //@ c@ see above on ping.  acks for motor ports seem to fail much less often (smaller sample size in my tests, though,
+  // but the extension actually ignores failures here.  so removing ack validation for these 3 commands
+  // changes nothing functionally. CEB 7/7/13
   def talkToOutputPorts(outputPortMask: Int) {
     writeAndWait(CmdTalkToOutputPort, outputPortMask.toByte)
   }
